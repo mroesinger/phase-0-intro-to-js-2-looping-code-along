2242cfc16d30999862461328038790adb5dd082a
michaelroesinger <mroesinger@gmail.com>
2022-07-21 13:27:17 -0700

    complete

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
-function writeCards( namesArray, event ) {
-  let thankYouCards = []
-  for ( let i = 0; i < namesArray.length; i++ ) {
-    thankYouCards.push( `Thank you, ${namesArray[i]}, for the wonderful ${event} gift!` )
+const cards = [];
+
+function writeCards(cards) {
+  let array = [];
+  for (let i = 0; i < cards.length; i++) {
+    array.push(`Thank you, ${cards[i]}, for the wonderful surprise gift!`);
   }
-  return thankYouCards
+
+  return array;
 }
 
-function countDown( startingNumber ) {
-  while ( startingNumber > 0 ) {
-    console.log( startingNumber );
+console.log(writeCards(["Guadalupe", "Ollie", "Aki"]));
+
+function countDown(startingNumber) {
+  while (startingNumber > 0) {
+    console.log(startingNumber);
     startingNumber -= 1;
   }
-  console.log( startingNumber );
+  console.log(startingNumber);
 }
